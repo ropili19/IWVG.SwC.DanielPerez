ebe0716a1e65e9c1e8afa2e8af4850bdee334622
Daniel Pérez <>
2018-09-21 09:49:26 +0200

    - Closes #8

--- src/test/java/es/upm/miw/iwvg/forge/junit/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg/forge/junit/FractionTest.java
@@ -1,10 +1,34 @@
 package es.upm.miw.iwvg.forge.junit;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FractionTest {
+
+    private Fraction fraction;
+
+    @BeforeEach
+    void before() {
+        fraction = new Fraction(1, 2);
+    }
+
+    @Test
+    void testDecimal() {
+        assertEquals(0.5, this.fraction.decimal());
+    }
+
+    @Test
+    void testNumerator() {
+        assertEquals(1, this.fraction.getNumerator());
+    }
+
+    @Test
+    void testDenominator() {
+        assertEquals(2, this.fraction.getDenominator());
+    }
 
     @Test
     void testIsPropia() {
